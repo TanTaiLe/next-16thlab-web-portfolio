23dd078e805cb4e278d1043d93202f150c386c66
TanTaiLe <33871424+TanTaiLe@users.noreply.github.com>
2025-09-11 02:11:13 +0700

    Initial commit

---
+++ components/Hello.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+function Hello() {
+  return <h1>Hello 16thLab</h1>;
+}
+
+describe('Hello component', () => {
+  it('renders correctly', () => {
+    render(<Hello />);
+    expect(screen.getByText('Hello 16thLab')).toBeInTheDocument();
+  });
+});
---
+++ tests/jest.config.mjs
@@ -0,0 +1,21 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './', // thư mục root của Next.js app
+})
+
+const customJestConfig = {
+  preset: 'ts-jest',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1', // alias @/ trỏ về thư mục gốc
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+  },
+}
+
+export default createJestConfig(customJestConfig)
